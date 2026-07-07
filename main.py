06619f630f4383e81b78801f13654fbbf3628b0b
Hariom Kumar <apple.aryan.620501@gmail.com>
2026-07-07 12:40:27 +0530

    Ready for Render deployment

--- main.py
+++ main.py
@@ -34,4 +34,5 @@ async def remove_bg(request: Request, file: UploadFile = File(...)):
     })
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
